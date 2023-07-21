316fbba877abb9e2dbb652c137dd8598a06490be
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2023-07-21 22:26:46 +0300

    Update router.js

--- router/router.js
+++ router/router.js
@@ -6,7 +6,7 @@ import *as controller from '../controllers/controllers.js'
 
 const router=Router();
 router.route('/questions')
-.get(controller.getqestions)
+.get(checkAuthentication,controller.getqestions)
 .post(Authadmin,controller.insertqestions)
 .put(Authadmin,controller.updatequestion)
 
