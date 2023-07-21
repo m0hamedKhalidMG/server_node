ba0a1d4063cf9ba7ba628bc36a9b7f98a4f3617e
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2023-07-21 15:58:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ config()
 app.use(cookieParser());
 app.use(cors ({
 
-        origin:["https://thriving-cheesecake-335939.netlify.app"],
+        origin:["https://leafy-nasturtium-fd42da.netlify.app"],
 
     credentials: true
 }) );
@@ -32,7 +32,7 @@ app.use(
     })
   );
   app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', 'https://thriving-cheesecake-335939.netlify.app');
+        res.setHeader('Access-Control-Allow-Origin', 'https://leafy-nasturtium-fd42da.netlify.app');
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
