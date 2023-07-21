b44f4d880984dad8eb5a1220b3865baaad637225
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2023-07-21 15:19:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,8 @@ config()
 app.use(cookieParser());
 app.use(cors ({
 
-    
+        origin:["http://localhost:3000"],
+
     credentials: true
 }) );
 app.use(
@@ -31,6 +32,8 @@ app.use(
     })
   );
   app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
