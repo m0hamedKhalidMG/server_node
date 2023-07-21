e38bc4d01b723e0b0c7a85900e4fb1f9f424fe23
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2023-07-21 16:11:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -20,8 +20,7 @@ config()
 app.use(cookieParser());
 app.use(cors ({
 
-        origin:["https://leafy-nasturtium-fd42da.netlify.app"],
-
+  origin: '*',
     credentials: true
 }) );
 app.use(
@@ -32,8 +31,7 @@ app.use(
     })
   );
   app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', 'https://leafy-nasturtium-fd42da.netlify.app');
-
+  res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
