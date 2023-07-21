ea92a742b6ba05974843e5c5ac255b93af5d41fd
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2023-07-21 14:30:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ config()
 app.use(cookieParser());
 app.use(cors ({
 
-    origin:["https://charming-bunny-0b11b9.netlify.app"],
+    origin:["https://tangerine-faloodeh-f2dfc9.netlify.app"],
     credentials: true
 }) );
 app.use(
@@ -31,7 +31,7 @@ app.use(
     })
   );
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://charming-bunny-0b11b9.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://tangerine-faloodeh-f2dfc9.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
