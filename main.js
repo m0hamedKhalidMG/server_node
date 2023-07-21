68025f29084cd132072ebddf07dc9f5fcb9f617a
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2023-07-21 17:24:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ config()
 app.use(cookieParser());
 app.use(cors ({
 
-  origin: '*',
+  origin: ['https://my345Quiz.netlify.app'],
     credentials: true
 }) );
 app.use(
@@ -31,7 +31,7 @@ app.use(
     })
   );
   app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://my345Quiz.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
