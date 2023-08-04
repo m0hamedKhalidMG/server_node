3fa391fb3bf4b07ce9d9dfadd9345b51d27c5234
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2023-08-05 02:30:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ config()
 app.use(cookieParser());
 app.use(cors ({
 
-    origin:["http://localhost:3000"],
+    origin:["https://symphonious-halva-343e09.netlify.app"],
     credentials: true
 }) );
 app.use(
@@ -42,7 +42,7 @@ app.use(
     })
   );
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://symphonious-halva-343e09.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
