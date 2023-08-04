f46fc4c57d21afe2daf2bd556962bf4bcc16f846
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2023-08-05 02:53:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ app.use(
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://symphonious-halva-343e09.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
   });
