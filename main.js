accc1d3dd12885fd9f1a8676f79f526dbaf847ab
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2023-08-05 04:10:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ config()
 app.use(cookieParser());
 app.use(cors ({
 
-    origin:["https://lambent-granita-4dcead.netlify.app"],
+    origin:"*",
     credentials: true
 }) );
 app.use(
@@ -31,7 +31,7 @@ app.use(
     })
   );
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://lambent-granita-4dcead.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
